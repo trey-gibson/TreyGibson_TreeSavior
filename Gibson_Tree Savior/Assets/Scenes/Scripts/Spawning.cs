b579114a62a7483f97473c94777f1414905f5f86
Student <Student@aii.stu>
2018-07-26 21:07:07 -0400

    Block out assets added
    Block out assets added

---
+++ Gibson_Tree Savior/Assets/Scenes/Scripts/Spawning.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 5; i++)
+        {
+            Instantiate(GameObject)TreeTrunk)
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

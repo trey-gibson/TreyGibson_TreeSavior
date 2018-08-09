24ce68bee0680edb8285ec31ed03df2b499048fa
trey-gibson <treycgibson@yahoo.com>
2018-08-09 12:37:18 -0400

    player added w/movement

--- Gibson_Tree Savior/Assets/Scripts/PlayerController.cs
+++ Gibson_Tree Savior/Assets/Scripts/PlayerController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-
+    public GameObject player;
+    public Vector3 RotationLeftValue;
+    public Vector3 RotationRightValue;
+ 
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,18 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Rotate(RotationLeftValue * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Rotate(RotationRightValue * Time.deltaTime);
+
+        }
+
+    }
 }
